bb7ba37fb84414338c6943fcabf37509329a0891
Murilo Molina Barone <murilo.m.barone@gmail.com>
2026-05-02 20:41:35 -0300

    feat: Autenticacao e autorizacao (ACL)

--- README.md
+++ README.md
@@ -9,6 +9,12 @@ Este broker atua como a primeira camada de recepção, gerenciando a telemetria 
 | **Go (Golang)** | Alta performance em concorrência (*goroutines*), essencial para lidar com múltiplos sensores simultâneos com baixo consumo de recursos e binários leves. |
 | **Mochi MQTT v2** | Biblioteca de broker *embeddable* que permite customização total via **Hooks**, permitindo injetar lógica de monitoramento e integração diretamente no motor do servidor. |
 
+### O que isso resolve?
+* **Reprodutibilidade:** Consegue rodar o broker com um único comando Docker, sem precisar instalar o Go na máquina.
+* **Profissionalismo:** O binário estático no `alpine` resulta em uma imagem de apenas alguns megabytes, mostrando eficiência.
+
+
+
 ## Arquitetura de Software (Hooks)
 
 O projeto utiliza o conceito de **Hooks** para interceptar pacotes sem interromper o fluxo principal do protocolo:
@@ -55,11 +61,34 @@ Conecte qualquer cliente MQTT (MQTTX, MQTT Explorer ou ESP32) ao endereço:
 
 - **Porta**: ``1883``
 
-Ao publicar uma mensagem, o terminal (ou logs do docker) exibirá:
-``Mensagem no tópico [fazenda/sensor1]: {"umidade": 45.2}``
+-----
+-----
+_______
+
+## O Broker recebe o seguinte padrao de mensagem
+`` mosquitto_pub -h <ip_do_broker> -i "<ID_de_quem_envia podendo ser o deviceId>" -u "<tipo_de_remetente>" -P "<senha_do_user>" -t "campo/<id_do_campo>/<tipo_de_remetente>/<deviceId>/dados" -m {payload:"payload"}`` 
+
+(tipo_de_rementente === sensor)
+
+``mosquitto_pub -h localhost -i "1e23456" -u "sensor" -P "123" -t "campo/fazenda1/sensor/1e23456/dados" -m {payload:"payload"}``
+
+(-i === deviceId)
+(-u === userName)
+(-P === senha do usuario)
+(-t === Topico)
+(-m === payload)
 
 
-### O que isso resolve?
-* **Reprodutibilidade:** Consegue rodar o broker com um único comando Docker, sem precisar instalar o Go na máquina.
-* **Profissionalismo:** O binário estático no `alpine` resulta em uma imagem de apenas alguns megabytes, mostrando eficiência.
+### Topic 
+``campo/{campoId}/sensor/{deviceId}/dados``
+
+##### Exemplo:
+
+``campo/123/sensor/1e23456/dados``
+
+### Payload
+##### Ideia de chaves exemplo:
+
+`` {timestamp, temp_ar, hum_ar, hum_solo, luminosidade, bateria (?), lat, long, flag_validade}``
+
 
--- main.go
+++ main.go
@@ -5,22 +5,26 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"strings"
 
 	"github.com/mochi-mqtt/server/v2"
-	"github.com/mochi-mqtt/server/v2/hooks/auth"
+	// "github.com/mochi-mqtt/server/v2/hooks/auth"
 	"github.com/mochi-mqtt/server/v2/listeners"
 	"github.com/mochi-mqtt/server/v2/packets" // Importante para reconhecer o tipo 'packets.Packet'
 )
 
 func main() {
 	server := mqtt.New(nil)
-	_ = server.AddHook(new(auth.AllowHook), nil) // qualquer cliente pode se conectar, sem autenticação // lembrar de trancar essas portas depois que tiver o banco e o esp funcionando
+	err := server.AddHook(new(AuthHook), nil)
+    if err != nil {
+        log.Fatal(err)
+    }
 
 	// Hook de log direto no main para não precisar de outro arquivo
 	_ = server.AddHook(&LogHook{}, nil)
 
 	tcp := listeners.NewTCP(listeners.Config{
-		ID:      "t1",
+		ID:      "mqtt-tcp",
 		Address: ":1883",
 	})
 	
@@ -43,7 +47,7 @@ func main() {
 	server.Close()
 }
 
-// --- HOOK para log ---
+// HOOK para log
 
 type LogHook struct {
 	mqtt.HookBase
@@ -59,4 +63,95 @@ func (h *LogHook) Provides(b byte) bool {
 
 func (h *LogHook) OnPublished(cl *mqtt.Client, pk packets.Packet) {
 	log.Printf("Mensagem no tópico [%s]: %s\n", pk.TopicName, string(pk.Payload))
+}
+
+
+// HOOK para autenticação
+
+type AuthHook struct {
+	mqtt.HookBase
+}
+
+func (h *AuthHook) ID() string {
+	return "mqtt-broker-auth-hook"
+}
+
+// este Hook cuida da Conexão (Auth) e Acesso a Tópicos (ACL)
+func (h *AuthHook) Provides(b byte) bool {
+	return b == mqtt.OnConnectAuthenticate || b == mqtt.OnACLCheck
+}
+
+// AUTENTICAÇÃO: Verifica Usuário e Senha
+func (h *AuthHook) OnConnectAuthenticate(cl *mqtt.Client, pk packets.Packet) bool {
+	// Vou validar com variáveis de ambiente 
+	user := string(pk.Connect.Username)
+	pass := string(pk.Connect.Password)
+
+	if user == "admin" && pass == "admin" {
+		return true // Admin tem acesso total
+	}
+
+	if user == "sensor" && pass == "123" {
+		return true
+	}
+
+	if user == "mqtt_sub" && pass == "mqtt_sub" {
+		return true
+	}
+
+	log.Printf("Conexão negada: Usuário %s incorreto", user)
+	return false
+}
+
+// ACL: Verifica se o cliente pode publicar/assinar em um tópico
+func (h *AuthHook) OnACLCheck(cl *mqtt.Client, topic string, write bool) bool {
+	// Regra de Ouro: Admin pode tudo
+	if string(cl.Properties.Username) == "admin" {
+		return true
+	}
+
+	// Regra para Sensores: Só podem publicar no seu próprio prefixo
+	// Exemplo: O cliente "esp32" só publica em "campo/+/sensor/<cliente_id>/dados"
+	if string(cl.Properties.Username) == "sensor" {
+		if write {
+			topic = strings.TrimSuffix(topic, "/")
+
+			parts := strings.Split(topic, "/")
+
+			log.Printf("DEBUG ACL -> topic=%s parts=%v", topic, parts)
+
+			// Esperado: campo/<algo>/sensor/<clientID>/dados
+			if len(parts) != 5 {
+				return false
+			}
+
+			if parts[0] != "campo" {
+				return false
+			}
+
+			if parts[2] != "sensor" {
+				return false
+			}
+
+			if parts[3] != cl.ID {
+				return false
+			}
+
+			if parts[4] != "dados" {
+				return false
+			}
+
+			log.Printf("WRITE OK sensor [%s] topic [%s]", cl.ID, topic)
+			return true
+		}
+
+
+		// leitura bloqueada
+		if !write {
+			return true
+		}
+	}
+
+	// Regra de Leitura (Subscribe): Impedir que sensores leiam dados de outros
+	return false 
 }
